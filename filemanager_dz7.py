8d098f326faa52cf21110b9bc2c08d8e23ea5735
anastasiya-h79 <anastasiya-h79@yandex.ru>
2019-11-28 17:30:03 +0300

    add save files to filemanager

---
+++ filemanager_dz7.py
@@ -0,0 +1,87 @@
+
+#import sys
+#import json
+
+import os
+import shutil
+from banc_account import buy
+from victory import victory
+
+curr_path = os.getcwd()
+
+
+def create_dir():
+    new_path = input('Введите имя папки')
+
+    if os.path.exists(new_path):
+        print('Папка уже существует!')
+    else:
+        os.mkdir(new_path)
+        print('Создана папка '+ new_path)
+    return new_path
+
+
+def delete_file_dir():
+    new_path = input('Введите имя папки/файла для удаления')
+    if not os.path.exists(new_path):
+        message = 'Папка/файл не существует!'
+    elif os.path.isdir(new_path):
+        shutil.rmtree(new_path)       #удалить каталог
+        message = 'Удалена папка: '+ new_path
+    else:
+        os.remove(os.path.join(curr_path, new_path))           #удалить файл
+        message = 'Удален файл: '+ new_path
+    return message
+
+
+def copy_file_dir():
+    in_path = input('Введите имя папки/файла для копирования ')
+    if not os.path.exists(in_path):
+        message = 'Копируемая папка/файл не существует!'
+    elif os.path.isdir(in_path):
+        shutil.copytree(in_path, os.getcwd())
+        message = 'Создана копия папки: '+ in_path
+    else:
+        os.path.isdir(in_path)
+        shutil.copyfile(in_path, os.getcwd())
+        message = 'Создана копия файла: '+ in_path
+    return message
+
+def show_list():
+    print(os.listdir())   #список файлов и директорий в папке
+
+def show_dir():
+    print('Папки: ', list(filter(lambda x: os.path.isdir(x), os.listdir())))
+
+def show_files():
+    print('Файлы:', list(filter(lambda x: os.path.isfile(x), os.listdir())))
+
+def show_os():
+    print('Операционная система:', os.name)
+
+def author_info():
+    print(os.getlogin())
+
+def victory_game():
+    victory()
+
+def banc_acc():
+    buy()
+
+
+def save_content():
+    content = os.listdir()
+    files = filter(lambda x: os.path.isfile(x), content)
+    dirs = filter(lambda x: os.path.isdir(x), content)
+    with open('listdir.txt', 'w') as f:
+        f.write('files: ')
+        for file in files:
+            f.write(f'{file}, ')
+        f.write('\n')
+        f.write('dirs: ')
+        for dir in dirs:
+            f.write(f'{dir}, ')
+
+
+
+
